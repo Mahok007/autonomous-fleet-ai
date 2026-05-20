dd1e89b327951fee38708cb4663afc28c446ebe9
Sarthak Sarkar <makokthekingsarthak@gmail.com>
2026-05-20 14:30:25 +0530

    Fix MongoDB SSL and requirements

--- backend/mongo_db.py
+++ backend/mongo_db.py
@@ -1,11 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL="mongodb+srv://makokthekingsarthak_db_user:borjXqBOT4vTJ4WU@cluster0.nuxrzde.mongodb.net/?appName=Cluster0"
+MONGO_URL = "mongodb+srv://makokthekingsarthak_db_user:borjXqBOT4vTJ4WU@cluster0.nuxrzde.mongodb.net/?appName=Cluster0"
 
-client=AsyncIOMotorClient(
-    MONGO_URL
+client = AsyncIOMotorClient(
+    MONGO_URL,
+    tls=True,
+    tlsAllowInvalidCertificates=True
 )
 
-db=client["fleet_ai"]
-
-users=db["users"]
+db = client["fleet_ai"]
+users = db["users"]
